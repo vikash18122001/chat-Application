f39ddb65ffe64e6431ac9364338c386352ea8d98
vikash maurya <vikashghazipur2001@gmail.com>
2023-01-06 22:42:30 +0530

    first commit

---
+++ js/client.js
@@ -0,0 +1,38 @@
+const socket=io('http://localhost:8000');
+const form=document.getElementById('send-container');
+const messageInput=document.getElementById('messageInp');
+const messageContainer=document.querySelector(".container");
+
+
+const audio=new Audio('Message notification.mp3');
+const append=(message,position)=>{
+    const messageElement=document.createElement('div');
+    messageElement.innerText=message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left')
+    {
+        audio.play();
+    }
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message=messageInput.value;
+    append(`You: ${message}`,'right');
+    socket.emit('send',message);
+    messageInput.value='';
+});
+const names=prompt("Enter your name to join " );
+socket.emit('new-user-joined',names);
+socket.on('user-joined', names=>{
+  append(`${names}  joined the chat`,'right');
+});
+socket.on('receive',data=>{
+    append(`${data.name}: ${data.message}`,'left');
+});
+socket.on('left',data=>{
+    append(`${data.name}: leave the chat`,'left');
+});
+ 
